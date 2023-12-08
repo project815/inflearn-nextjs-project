8245eb9c6e321da2360a9ac87571bdeeac4f0c62
songminseok <ske3056@gmail.com>
2023-12-08 15:06:20 +0900

    feat:board init

--- src/pages/board/register/index.tsx
+++
@@ -1,9 +0,0 @@
-import BoardRegister from "@/components/register/BoardRegister.container";
-
-export default function RegisterBoardPage() {
-  return (
-    <>
-      <BoardRegister />
-    </>
-  );
-}
