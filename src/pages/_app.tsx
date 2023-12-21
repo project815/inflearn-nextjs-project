2e16ac0bd255ef2eb7034cff6d2ad8f1a5707f9c
songminseok <ske3056@gmail.com>
2023-12-21 22:48:32 +0900

    feat: 폰트 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import Layout from "@/components/layout";
 import "@/styles/globals.css";
+import { GlobalStyles } from "@/styles/globalscss";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { Global } from "@emotion/react";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
@@ -10,9 +12,12 @@ export default function App({ Component, pageProps }: AppProps): JSX.Element {
   });
   return (
     <ApolloProvider client={client}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <>
+        <Global styles={GlobalStyles}></Global>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </>
     </ApolloProvider>
   );
 }
---
+++ src/styles/globalscss/index.tsx
@@ -0,0 +1,11 @@
+import { css } from "@emotion/react";
+
+export const GlobalStyles = css`
+  * {
+    font-family: "NotoSansKR";
+  }
+  @font-face {
+    font-family: "NotoSansKR";
+    src: url("/font/NotoSansKR-Medium.ttf");
+  }
+`;
